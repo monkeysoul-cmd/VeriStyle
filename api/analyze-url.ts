1222b81565b2342dc975df1d00deded70f5b6fbe
ayushbhati <ayushrawal2003@gmail.com>
2026-08-31 21:07:13 +0530

    fix: set charset=utf-8 Content-Type header for Rupee symbol

--- api/analyze-url.ts
+++ api/analyze-url.ts
@@ -486,6 +486,7 @@ export default async function handler(req: any, res: any) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
